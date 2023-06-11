a0f983c188208c35fc390a02bbe9925a0a096492
PEYZ <zhiani2000@gmail.com>
2023-06-11 16:22:25 -0700

    initial end-end folder structure

---
+++ schedulerApp-server/fic-scheduler/src/main/java/com/ficproject/ficscheduler/FicSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.ficproject.ficscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FicSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FicSchedulerApplication.class, args);
+	}
+
+}
---
+++ schedulerApp-server/fic-scheduler/src/test/java/com/ficproject/ficscheduler/FicSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ficproject.ficscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FicSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
