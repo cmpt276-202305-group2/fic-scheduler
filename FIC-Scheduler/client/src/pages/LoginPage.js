c69618d1cc954122aa05cae3e897ca0a50c9d284
PEYZ <zhiani2000@gmail.com>
2023-06-29 22:06:12 -0700

    changed Folder Structure

---
+++ FIC-Scheduler/client/src/pages/LoginPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import  styles  from "./LoginPage.module.css";
+
+function LoginPage() {
+  return <div className={styles.pageBody}>LoginPage</div>;
+}
+
+export default LoginPage;
