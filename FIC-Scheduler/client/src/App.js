fe02a78ddd4e0178acde9edce67a057e3812365f
PEYZ <zhiani2000@gmail.com>
2023-07-03 14:45:29 -0700

    -

--- FIC-Scheduler/client/src/App.js
+++ FIC-Scheduler/client/src/App.js
@@ -1,19 +1,20 @@
 import "./App.css";
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import InstructorHomePage from "./pages/InstructorHomePage";
 import CoordinatorHomePage from "./pages/CoordinatorHomePage";
+import React from "react";
 
 function App() {
-  return <div className="App">
-    <>
+  return (
+    <div className="App">
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/InstructorHomePage" element={<InstructorHomePage />} />
         <Route path="/CoordinatorHomePage" element={<CoordinatorHomePage />} />
       </Routes>
-    </>
-  </div>;
+    </div>
+  );
 }
 
 export default App;
