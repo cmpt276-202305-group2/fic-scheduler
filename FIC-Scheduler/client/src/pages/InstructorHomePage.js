daf6619275ca2360e672f345893f54c3021c3e6c
jlunder <joe.lunderville@gmail.com>
2023-07-03 13:54:45 -0700

    Add setup docs (#18)
    * Add basic setup docs
    
    * Doc formatting
    
    * fixed build issue
    
    ---------
    
    Co-authored-by: PEYZ <zhiani2000@gmail.com>

--- FIC-Scheduler/client/src/pages/InstructorHomePage.js
+++ FIC-Scheduler/client/src/pages/InstructorHomePage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./InstructorCoordinatorHomePage.module.css";
 import InstructorSidebar from "../components/InstructorSidebar";
 
