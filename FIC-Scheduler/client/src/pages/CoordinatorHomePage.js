71c9a0f5b8fed3cc085ebe142ee52af43312cd3b
PEYZ <zhiani2000@gmail.com>
2023-07-03 13:43:45 -0700

    made temp changes will add later

--- FIC-Scheduler/client/src/pages/CoordinatorHomePage.js
+++ FIC-Scheduler/client/src/pages/CoordinatorHomePage.js
@@ -3,18 +3,16 @@ import styles from "./InstructorCoordinatorHomePage.module.css";
 import CoordinatorSidebar from "../components/CoordinatorSidebar";
 
 function CoordinatorHomePage() {
-    return (
-        <>
-        <div className = {styles.Container}>
-        <div className = {styles.Sidebar}>
-            <CoordinatorSidebar className = {styles.Sidebar}/>
+  return (
+    <>
+      <div className={styles.Container}>
+        <div className={styles.Sidebar}>
+          <CoordinatorSidebar className={styles.Sidebar} />
         </div>
-        <div className = {styles.Schedule}>
-            SCHEDULE GOES HERE
-        </div>
-        </div>
-        </>   
-    );
+        <div className={styles.Schedule}>SCHEDULE GOES HERE</div>
+      </div>
+    </>
+  );
 }
 
 export default CoordinatorHomePage;
--- FIC-Scheduler/client/src/pages/InstructorHomePage.js
+++ FIC-Scheduler/client/src/pages/InstructorHomePage.js
@@ -1,20 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./InstructorCoordinatorHomePage.module.css";
 import InstructorSidebar from "../components/InstructorSidebar";
 
 function InstructorHomePage() {
-    return (
-        <>
-        <div className = {styles.Container}>
-        <div className = {styles.Sidebar}>
-            <InstructorSidebar className = {styles.Sidebar}/>
+  return (
+    <React.Fragment>
+      <div className={styles.Container}>
+        <div className={styles.Sidebar}>
+          <InstructorSidebar className={styles.Sidebar} />
         </div>
-        <div className = {styles.Schedule}>
-            SCHEDULE GOES HERE
-        </div>
-        </div>
-        </>   
-    );
+        <div className={styles.Schedule}>SCHEDULE GOES HERE</div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default InstructorHomePage;
