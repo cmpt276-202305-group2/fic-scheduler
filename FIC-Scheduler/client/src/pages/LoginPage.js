9f3b2de08851c275884151b4cdd5d594c39bd521
msblee123 <62529126+Picelu@users.noreply.github.com>
2023-07-02 20:03:43 -0700

    Login page updated

--- FIC-Scheduler/client/src/pages/LoginPage.js
+++ FIC-Scheduler/client/src/pages/LoginPage.js
@@ -1,8 +1,52 @@
-import React from "react";
-import  styles  from "./LoginPage.module.css";
+import React, { useState } from "react";
+
+import axios from "axios";
 
 function LoginPage() {
-  return <div className={styles.pageBody}>LoginPage</div>;
+  // set up the user
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      //send req
+      const res = await axios.post("/login", { username, password });
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username
+          <input
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+
+        <br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
