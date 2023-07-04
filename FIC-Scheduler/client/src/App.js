33f1e50d9dfd78b220c85db4a99c9b58cb49e8a9
Peiman Zhiani <64120482+peyz21@users.noreply.github.com>
2023-07-04 14:12:35 -0700

    fixed the bug with multi user adding on the db, and fixed the logout navigation (#24)

--- FIC-Scheduler/client/src/App.js
+++ FIC-Scheduler/client/src/App.js
@@ -4,14 +4,18 @@ import LoginPage from "./pages/LoginPage";
 import InstructorHomePage from "./pages/InstructorHomePage";
 import CoordinatorHomePage from "./pages/CoordinatorHomePage";
 import React from "react";
+import LogoutPage from "./pages/LogoutPage";
+import { Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/InstructorHomePage" element={<InstructorHomePage />} />
         <Route path="/CoordinatorHomePage" element={<CoordinatorHomePage />} />
+        <Route path="/LogoutPage" element={<LogoutPage />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </div>
   );
