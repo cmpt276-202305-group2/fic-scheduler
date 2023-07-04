d043cbc8dc2f4d6e6e85bc89b5cf897f462589e4
PEYZ <zhiani2000@gmail.com>
2023-07-04 14:07:15 -0700

    fixed the bug with multi user adding on the db, and fixed the logout navigation

---
+++ FIC-Scheduler/client/src/pages/LogoutPage.js
@@ -0,0 +1,35 @@
+import { Card } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import styles from "./LogoutPage.module.css";
+import { useNavigate } from "react-router-dom";
+
+function LogoutPage() {
+  const navigate = useNavigate();
+  const [timer, setTimer] = useState(9);
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      setTimer((prevTimer) => prevTimer - 1);
+    }, 1000);
+
+    const redirect = setTimeout(() => {
+      navigate("/login");
+    }, 9000);
+
+    return () => {
+      clearTimeout(redirect);
+      clearInterval(countdown);
+    };
+  }, [navigate]);
+
+  return (
+    <div className={styles.container}>
+      <Card className={styles.cardContainer}>
+        <h2>You have Successfully logged out</h2>
+        <p>Redirecting in {timer} seconds...</p>
+      </Card>
+    </div>
+  );
+}
+
+export default LogoutPage;
