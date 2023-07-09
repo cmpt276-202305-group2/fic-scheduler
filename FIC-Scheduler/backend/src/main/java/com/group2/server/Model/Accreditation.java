d7691da2818284fde85745e171702a00cb017255
regi-zzzzz <85130577+regi-zzzzz@users.noreply.github.com>
2023-07-09 12:44:13 -0700

    Added Classes to Model according to UML diagram (#70)

---
+++ FIC-Scheduler/backend/src/main/java/com/group2/server/Model/Accreditation.java
@@ -0,0 +1,23 @@
+package com.group2.server.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "Accreditaion")
+public class Accreditation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;  
+
+    @Getter
+    @Setter
+    private String name;
+    
+}
---
+++ FIC-Scheduler/backend/src/main/java/com/group2/server/Model/Classroom.java
@@ -0,0 +1,38 @@
+package com.group2.server.Model;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Classroom")
+public class Classroom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+
+    @Getter
+    @Setter
+    private String roomNumber;
+
+    @Getter
+    @Setter
+    @ManyToMany
+    @JoinTable(name="Classroom_Facilities",
+        joinColumns=
+            @JoinColumn(name="Classroom_ID", referencedColumnName="ID"),
+        inverseJoinColumns=
+            @JoinColumn(name="Facilities_ID", referencedColumnName="ID")
+        )
+    private Set<Facilities> facilitiesAvailable;
+    
+}
