fc1def6c2cc4c6a0f21b9d071a254627206b0549
regi-zzzzz <85130577+regi-zzzzz@users.noreply.github.com>
2023-07-09 12:36:04 -0700

    Added Classes to Model according to UML diagram

---
+++ FIC-Scheduler/backend/src/main/java/com/group2/server/Model/Instructor.java
@@ -0,0 +1,38 @@
+package com.group2.server.Model;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Instructor")
+public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    @ManyToMany
+    @JoinTable(name="Instructor_Accreditation",
+        joinColumns=
+            @JoinColumn(name="Instructor_ID", referencedColumnName="ID"),
+        inverseJoinColumns=
+            @JoinColumn(name="Accreditation_ID", referencedColumnName="ID")
+        )
+    private Set<Accreditation> accreditation;
+}
