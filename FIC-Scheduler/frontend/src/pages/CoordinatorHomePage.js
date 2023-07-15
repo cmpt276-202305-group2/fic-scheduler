2c834d63c35dbf40508d4c0a7b27ce1954dbb176
msblee123 <62529126+Picelu@users.noreply.github.com>
2023-07-15 13:52:36 -0700

    added excel view for coordinator

--- FIC-Scheduler/frontend/src/pages/CoordinatorHomePage.js
+++ FIC-Scheduler/frontend/src/pages/CoordinatorHomePage.js
@@ -1,15 +1,18 @@
 import React from "react";
 import styles from "./InstructorCoordinatorHomePage.module.css";
 import CoordinatorSidebar from "../components/CoordinatorSidebar";
+import ExcelViewer from "../components/ExcelViewer";
 
 function CoordinatorHomePage() {
   return (
     <React.Fragment>
       <div className={styles.Container}>
         <div className={styles.Sidebar}>
-          <CoordinatorSidebar /> 
+          <CoordinatorSidebar />
         </div>
-        <div className={styles.Schedule} data-testid="schedule">SCHEDULE GOES HERE</div>
+        <div className={styles.Schedule} data-testid="schedule">
+          <ExcelViewer />
+        </div>
       </div>
     </React.Fragment>
   );
