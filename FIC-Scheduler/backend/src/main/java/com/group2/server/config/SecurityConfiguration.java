d1d3550271b0b593674418f9d35e5b92e950fac9
Joseph Lunderville <joe.lunderville@gmail.com>
2023-07-16 02:01:26 -0700

    Fix CORS issue: permit preflight requests

--- FIC-Scheduler/backend/src/main/java/com/group2/server/config/SecurityConfiguration.java
+++ FIC-Scheduler/backend/src/main/java/com/group2/server/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -52,6 +53,7 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults());
         return http
                 .csrf(scrf -> scrf.disable())
                 .authorizeHttpRequests(auth -> {
