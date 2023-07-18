7843f7b2a7d7e36365e4e461efe8481751095cb8
Joseph Lunderville <joseph_lunderville@sfu.ca>
2023-07-18 13:56:41 -0700

    Missed commit

--- FIC-Scheduler/frontend/src/App.js
+++ FIC-Scheduler/frontend/src/App.js
@@ -19,7 +19,7 @@ var setUserInfo = (_) => { console.log('dummy setUserInfo'); };
 export const UserInfoContext = createContext();
 export const validRoles = new Set('ADMIN', 'COORDINATOR', 'INSTRUCTOR')
 export const storeUserInfo = async (userInfoDto) => {
-  console.log("storeUserInfo");
+  // console.log("storeUserInfo");
   const userInfo = {
     username: '' + (userInfoDto.username ?? 'nouser'),
     roles: Array.from(userInfoDto.roles ?? []),
@@ -27,10 +27,10 @@ export const storeUserInfo = async (userInfoDto) => {
   };
   localStorage.setItem('userInfo', JSON.stringify(userInfoDto));
   setUserInfo(userInfo);
-  console.log("storeUserInfo done");
+  // console.log("storeUserInfo done");
 };
 export const fetchUserInfo = async () => {
-  console.log("fetchUserInfo");
+  // console.log("fetchUserInfo");
   try {
     const response = await axios.get("auth/userinfo", { withCredentials: true });
     if (response.status == 200) {
@@ -46,7 +46,7 @@ export const fetchUserInfo = async () => {
 };
 
 function App() {
-  console.log("App render");
+  // console.log("App render");
 
   axios.defaults.baseURL = "http://localhost:8080/";
   axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
