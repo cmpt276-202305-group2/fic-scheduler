6c78af6366bd845b3ac11189fa2ec3afc218d13d
Joseph Lunderville <joseph_lunderville@sfu.ca>
2023-07-18 09:28:10 -0700

    Fix problems with stale JWTs on login/logout

--- FIC-Scheduler/backend/src/main/java/com/group2/server/config/SecurityConfiguration.java
+++ FIC-Scheduler/backend/src/main/java/com/group2/server/config/SecurityConfiguration.java
@@ -1,28 +1,30 @@
 package com.group2.server.config;
 
+import com.group2.server.utils.RSAKeyProperties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.group2.server.utils.RSAKeyProperties;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
@@ -73,24 +75,43 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors(Customizer.withDefaults());
         http
-                .csrf(scrf -> scrf.disable())
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/auth/**").permitAll();
+                    // auth.anyRequest().permitAll();
+
+                    // /auth/** exceptions actually handled below, in the custom authentication
+                    // filter handler
+                    // auth.requestMatchers("/auth/**").permitAll();
                     auth.requestMatchers("/api/coordinator/**").hasRole("COORDINATOR");
                     auth.requestMatchers("/api/instructor/**").hasRole("INSTRUCTOR");
                     auth.anyRequest().authenticated();
                 })
                 .oauth2ResourceServer(
-                    oauth -> oauth.bearerTokenResolver(cookieBearerTokenResolver())
-                             .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
-                )
+                        oauth -> oauth.bearerTokenResolver(cookieBearerTokenResolver())
+                                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                                .withObjectPostProcessor(new ObjectPostProcessor<BearerTokenAuthenticationFilter>() {
+                                    // Reference for this issue at
+                                    // https://stackoverflow.com/questions/72943493/how-to-set-authentication-failure-handler-on-bearertokenauthenticationfilter
+                                    @Override
+                                    public <O extends BearerTokenAuthenticationFilter> O postProcess(O filter) {
+                                        filter.setAuthenticationFailureHandler((request, response, exception) -> {
+                                            if (request.getRequestURI().equals("/auth/login")
+                                                    || request.getRequestURI().equals("/auth/logout")) {
+                                                System.out.println(
+                                                        "Authentication failed for /auth/login or /auth/logout, hiding");
+                                            } else {
+                                                var delegate = new BearerTokenAuthenticationEntryPoint();
+                                                delegate.commence(request, response, exception);
+                                            }
+                                        });
+                                        return filter;
+                                    }
+                                }))
+
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
-    
-
-    
 
     @Bean
     public JwtDecoder jwtDecoder() {
