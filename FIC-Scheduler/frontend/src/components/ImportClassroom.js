a4ba24b21bda12096d11e7c4d3f0aba2162de5c5
Peiman Zhiani <64120482+peyz21@users.noreply.github.com>
2023-07-23 00:09:04 -0700

    added .env for frontend and moved backend's username and password inside applicaiton.properties (#102)

--- FIC-Scheduler/frontend/src/components/ImportClassroom.js
+++ FIC-Scheduler/frontend/src/components/ImportClassroom.js
@@ -5,96 +5,99 @@ import styles from "./FileImport.module.css";
 import { tokenConfig } from "../utils";
 import { FileUploader, SpreadsheetTable } from "./FileUploader";
 
-const ImportClassroom = ({ classroomSpreadsheetData, setClassroomSpreadsheetData }) => {
-    const [selectedFile, setSelectedFile] = useState(null);
-    const [showErrorMessage, setShowErrorMessage] = useState(false);
-    const [isPreviewVisible, setIsPreviewVisible] = useState(false);
+const ImportClassroom = ({
+  classroomSpreadsheetData,
+  setClassroomSpreadsheetData,
+}) => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
+  const [isPreviewVisible, setIsPreviewVisible] = useState(false);
 
-    const createFileUploadHandler =
-        (setFile, setErrorMessage, setData, setIsPreviewVisible) =>
-            async (event) => {
-                const file = event.target.files[0];
-                if (!file) return;
-                const allowedFormats = ["xlsx", "csv"];
-                const fileExtension = file.name.split(".").pop().toLowerCase();
-                if (!allowedFormats.includes(fileExtension)) {
-                    setErrorMessage(true);
-                    return;
-                }
-                setErrorMessage(false);
-                try {
-                    const data = await readExcelFile(file);
-                    setData(data);
-                    setFile(file.name);
-                    setIsPreviewVisible(true);
-                } catch (error) {
-                    console.error("Error reading Excel file:", error);
-                }
-            };
-
-    const handleFileUpload = createFileUploadHandler(
-        setSelectedFile,
-        setShowErrorMessage,
-        setClassroomSpreadsheetData,
-        setIsPreviewVisible
-    );
-
-    const handleSendToBackEnd = async () => {
-        if (classroomSpreadsheetData.length > 0) {
-            const jsonData = classroomSpreadsheetData.map((row) => {
-                return {
-                    roomNumber: row.roomNumber,
-                    facilitesAvaliableNames: [
-                        row.facilitesAvaliableNames1,
-                        row.facilitesAvaliableNames2,
-                        row.facilitesAvaliableNames3,
-                        row.facilitesAvaliableNames4,
-                        row.facilitesAvaliableNames5,
-                        row.facilitesAvaliableNames6,
-                        row.facilitesAvaliableNames7,
-                        row.facilitesAvaliableNames8,
-                        row.facilitesAvaliableNames9,
-                    ].filter(Boolean),
-                };
-            });
-
-            try {
-                const response = await axios.post(
-                    "http://localhost:8080/api/classrooms",
-                    jsonData,
-                    tokenConfig()
-                );
-
-                if (response.status === 200) {
-                    const result = response.json();
-                    console.log("File upload successful:", result);
-                } else {
-                    console.error("Error uploading Excel file:", response.statusText);
-                }
-            } catch (error) {
-                console.error("Error reading Excel file:", error);
-            }
-        }
+  const createFileUploadHandler =
+    (setFile, setErrorMessage, setData, setIsPreviewVisible) =>
+    async (event) => {
+      const file = event.target.files[0];
+      if (!file) return;
+      const allowedFormats = ["xlsx", "csv"];
+      const fileExtension = file.name.split(".").pop().toLowerCase();
+      if (!allowedFormats.includes(fileExtension)) {
+        setErrorMessage(true);
+        return;
+      }
+      setErrorMessage(false);
+      try {
+        const data = await readExcelFile(file);
+        setData(data);
+        setFile(file.name);
+        setIsPreviewVisible(true);
+      } catch (error) {
+        console.error("Error reading Excel file:", error);
+      }
     };
 
-    return (
-        <div className={styles.tableHolder}>
-            <h2 className={styles.title}>Classrooms</h2>
-            <FileUploader
-                selectedFile={selectedFile}
-                handleFileUpload={handleFileUpload}
-                showErrorMessage={showErrorMessage}
-                isPreviewVisible={isPreviewVisible}
-                handleSendToBackend={handleSendToBackEnd}
-                id="file-upload-one"
-                styles={styles}
-            />
-            <SpreadsheetTable
-                spreadsheetData={classroomSpreadsheetData}
-                styles={styles}
-            />
-        </div>
-    );
+  const handleFileUpload = createFileUploadHandler(
+    setSelectedFile,
+    setShowErrorMessage,
+    setClassroomSpreadsheetData,
+    setIsPreviewVisible
+  );
+
+  const handleSendToBackEnd = async () => {
+    if (classroomSpreadsheetData.length > 0) {
+      const jsonData = classroomSpreadsheetData.map((row) => {
+        return {
+          roomNumber: row.roomNumber,
+          facilitesAvaliableNames: [
+            row.facilitesAvaliableNames1,
+            row.facilitesAvaliableNames2,
+            row.facilitesAvaliableNames3,
+            row.facilitesAvaliableNames4,
+            row.facilitesAvaliableNames5,
+            row.facilitesAvaliableNames6,
+            row.facilitesAvaliableNames7,
+            row.facilitesAvaliableNames8,
+            row.facilitesAvaliableNames9,
+          ].filter(Boolean),
+        };
+      });
+
+      try {
+        const response = await axios.post(
+          `${process.env.REACT_APP_BACKEND_URL}/api/classrooms`,
+          jsonData,
+          tokenConfig()
+        );
+
+        if (response.status === 200) {
+          const result = response.json();
+          console.log("File upload successful:", result);
+        } else {
+          console.error("Error uploading Excel file:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error reading Excel file:", error);
+      }
+    }
+  };
+
+  return (
+    <div className={styles.tableHolder}>
+      <h2 className={styles.title}>Classrooms</h2>
+      <FileUploader
+        selectedFile={selectedFile}
+        handleFileUpload={handleFileUpload}
+        showErrorMessage={showErrorMessage}
+        isPreviewVisible={isPreviewVisible}
+        handleSendToBackend={handleSendToBackEnd}
+        id="file-upload-one"
+        styles={styles}
+      />
+      <SpreadsheetTable
+        spreadsheetData={classroomSpreadsheetData}
+        styles={styles}
+      />
+    </div>
+  );
 };
 
 export default ImportClassroom;
