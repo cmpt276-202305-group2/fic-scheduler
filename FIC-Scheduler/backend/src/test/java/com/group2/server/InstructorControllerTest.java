4b9789a1692c06cf07c63437f89f529b97e122e8
regi-zzzzz <85130577+regi-zzzzz@users.noreply.github.com>
2023-07-26 13:08:38 -0700

    Updated SemesterPlanControllerTest

--- FIC-Scheduler/backend/src/test/java/com/group2/server/InstructorControllerTest.java
+++ FIC-Scheduler/backend/src/test/java/com/group2/server/InstructorControllerTest.java
@@ -5,12 +5,15 @@ import com.group2.server.controller.InstructorController;
 import com.group2.server.dto.InstructorDto;
 import com.group2.server.model.Instructor;
 import com.group2.server.repository.InstructorRepository;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -25,6 +28,8 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 public class InstructorControllerTest {
 
     @InjectMocks
