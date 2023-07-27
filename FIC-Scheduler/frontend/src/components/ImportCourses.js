692118fc71fadbecff2f280001f6f9f899129069
FuJen0980 <chiangjeff23@gmail.com>
2023-07-26 19:15:16 -0700

    able to view classroom and instructor data

--- FIC-Scheduler/frontend/src/components/ImportCourses.js
+++ FIC-Scheduler/frontend/src/components/ImportCourses.js
@@ -2,105 +2,126 @@ import React, { useState } from "react";
 import axios from "axios";
 import readExcelFile from "./readExcelfile";
 import styles from "./FileImport.module.css";
+import Button from "@mui/material/Button";
+import { ViewUploadedCourseOfferingList } from "./ViewUploadedCourseOfferingList";
 import { tokenConfig } from "../utils";
 import { FileUploader, SpreadsheetTable } from "./FileUploader";
 
 const ImportCourses = ({
-  coursesSpreadsheetData,
-  setCoursesSpreadsheetData,
+    coursesSpreadsheetData,
+    setCoursesSpreadsheetData,
 }) => {
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [showErrorMessage, setShowErrorMessage] = useState(false);
-  const [isPreviewVisible, setIsPreviewVisible] = useState(false);
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [showErrorMessage, setShowErrorMessage] = useState(false);
+    const [isPreviewVisible, setIsPreviewVisible] = useState(false);
+    const [isCourseOfferingListVisible, setIsCourseOfferingListVisible] = useState(false);
 
-  const createFileUploadHandler =
-    (setFile, setErrorMessage, setData, setIsPreviewVisible) =>
-    async (event) => {
-      const file = event.target.files[0];
-      if (!file) return;
-      const allowedFormats = ["xlsx", "csv"];
-      const fileExtension = file.name.split(".").pop().toLowerCase();
-      if (!allowedFormats.includes(fileExtension)) {
-        setErrorMessage(true);
-        return;
-      }
-      setErrorMessage(false);
-      try {
-        const data = await readExcelFile(file);
-        setData(data);
-        setFile(file.name);
-        setIsPreviewVisible(true);
-      } catch (error) {
-        console.error("Error reading Excel file:", error);
-      }
+    const handleShowCourseOfferingList = () => {
+        setIsCourseOfferingListVisible(prevIsCourseOfferingListVisible => !prevIsCourseOfferingListVisible);
+    }
+
+    const createFileUploadHandler =
+        (setFile, setErrorMessage, setData, setIsPreviewVisible) =>
+            async (event) => {
+                const file = event.target.files[0];
+                if (!file) return;
+                const allowedFormats = ["xlsx", "csv"];
+                const fileExtension = file.name.split(".").pop().toLowerCase();
+                if (!allowedFormats.includes(fileExtension)) {
+                    setErrorMessage(true);
+                    return;
+                }
+                setErrorMessage(false);
+                try {
+                    const data = await readExcelFile(file);
+                    setData(data);
+                    setFile(file.name);
+                    setIsPreviewVisible(true);
+                } catch (error) {
+                    console.error("Error reading Excel file:", error);
+                }
+            };
+
+    const handleFileUpload = createFileUploadHandler(
+        setSelectedFile,
+        setShowErrorMessage,
+        setCoursesSpreadsheetData,
+        setIsPreviewVisible
+    );
+
+    const handleSendToBackEnd = async () => {
+        if (coursesSpreadsheetData.length > 0) {
+            // Convert data to JSON format
+            const jsonData = coursesSpreadsheetData.map((row) => {
+                return {
+                    id: null,
+                    courseNumber: row.courseNumber,
+                    semesterPlanID: row.semesterPlanId,
+
+                    approvedInstructor: [
+                        row.approvedInstructor1,
+                        row.approvedInstructor2,
+                    ].filter(Boolean),
+                };
+            });
+
+            try {
+                // Instead of sending the formData, we send jsonData as the request data
+                const response = await axios.post(
+                    `${process.env.REACT_APP_BACKEND_URL}/api/courseOffering`,
+                    jsonData,
+                    tokenConfig(),
+                    {
+                        headers: {
+                            "Content-Type": "application/json", // Set the appropriate content type for JSON data
+                        },
+                    }
+                );
+
+                if (response.status === 200) {
+                    const result = response.data;
+                    console.log("File upload successful:", result);
+                } else {
+                    console.error("Error uploading Excel file:", response.statusText);
+                }
+            } catch (error) {
+                console.error("Error sending JSON data to the backend:", error);
+            }
+        }
     };
 
-  const handleFileUpload = createFileUploadHandler(
-    setSelectedFile,
-    setShowErrorMessage,
-    setCoursesSpreadsheetData,
-    setIsPreviewVisible
-  );
-
-  const handleSendToBackEnd = async () => {
-    if (coursesSpreadsheetData.length > 0) {
-      // Convert data to JSON format
-      const jsonData = coursesSpreadsheetData.map((row) => {
-        return {
-          id: null,
-          courseNumber: row.courseNumber,
-          semesterPlanID: row.semesterPlanId,
-
-          approvedInstructor: [
-            row.approvedInstructor1,
-            row.approvedInstructor2,
-          ].filter(Boolean),
-        };
-      });
-
-      try {
-        // Instead of sending the formData, we send jsonData as the request data
-        const response = await axios.post(
-          `${process.env.REACT_APP_BACKEND_URL}/api/courseOffering`,
-          jsonData,
-          tokenConfig(),
-          {
-            headers: {
-              "Content-Type": "application/json", // Set the appropriate content type for JSON data
-            },
-          }
-        );
-
-        if (response.status === 200) {
-          const result = response.data;
-          console.log("File upload successful:", result);
-        } else {
-          console.error("Error uploading Excel file:", response.statusText);
-        }
-      } catch (error) {
-        console.error("Error sending JSON data to the backend:", error);
-      }
-    }
-  };
-
-  return (
-    <div className={styles.tableHolder}>
-      <h2 className={styles.title}>Course Offering</h2>
-      <FileUploader
-        selectedFile={selectedFile}
-        handleFileUpload={handleFileUpload}
-        showErrorMessage={showErrorMessage}
-        isPreviewVisible={isPreviewVisible}
-        handleSendToBackend={handleSendToBackEnd}
-        id={4}
-        styles={styles}
-      />
-      <SpreadsheetTable
-        spreadsheetData={coursesSpreadsheetData}
-        styles={styles}
-      />
-    </div>
-  );
+    return (
+        <div className={styles.tableHolder}>
+            <h2 className={styles.title}>Course Offering</h2>
+            <FileUploader
+                selectedFile={selectedFile}
+                handleFileUpload={handleFileUpload}
+                showErrorMessage={showErrorMessage}
+                isPreviewVisible={isPreviewVisible}
+                handleSendToBackend={handleSendToBackEnd}
+                id={4}
+                styles={styles}
+            />
+            <SpreadsheetTable
+                spreadsheetData={coursesSpreadsheetData}
+                styles={styles}
+            />
+            <Button
+                onClick={handleShowCourseOfferingList}
+                variant="contained"
+                color="primary"
+                sx={{
+                    color: "white",
+                    backgroundColor: "#417A1A",
+                    "&:hover": { backgroundColor: "#417A1A" },
+                }}
+                style={{ marginBottom: 10, marginTop: 10 }}
+            >
+                {isCourseOfferingListVisible ? "Hide" : "Show"} Current Course Offerings
+            </Button>
+            {isCourseOfferingListVisible && <ViewUploadedCourseOfferingList />}
+        </div>
+    );
 };
 
 export default ImportCourses;
---
+++ FIC-Scheduler/frontend/src/components/ViewUploadedCourseOfferingList.js
@@ -0,0 +1,154 @@
+import axios from 'axios';
+import React, { useEffect, useState } from "react";
+
+import Paper from "@mui/material/Paper";
+import TableContainer from "@mui/material/TableContainer";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import TableBody from "@mui/material/TableBody";
+import styles from "./FileImport.module.css";
+
+import { tokenConfig } from "../utils"
+
+export function ViewUploadedCourseOfferingList() {
+    const [allCourseOfferings, setAllCourseOfferings] = useState(null);
+    const [instructorsMap, setInstructorsMap] = useState({});
+
+    useEffect(() => {
+        // Fetch both course offerings and instructors
+        const fetchData = async () => {
+            try {
+                const [courseOfferingsResponse, instructorsResponse] = await Promise.all([
+                    axios.get("api/course-offerings", tokenConfig()),
+                    axios.get("api/instructors", tokenConfig()),
+                ]);
+
+                setAllCourseOfferings(courseOfferingsResponse.data);
+
+                const instructorsData = instructorsResponse.data;
+                const instructorsById = instructorsData.reduce((acc, instructor) => {
+                    acc[instructor.id] = instructor;
+                    return acc;
+                }, {});
+                console.log("Instructors Map:", instructorsById);
+                setInstructorsMap(instructorsById);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+                setAllCourseOfferings(null);
+                setInstructorsMap({});
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const getInstructorName = (instructorId) => {
+        return instructorsMap[instructorId]?.name || "N/A";
+    };
+
+    var data = (<h3>No Data</h3>);
+    if (((allCourseOfferings ?? null) !== null) && (allCourseOfferings instanceof Array)) {
+        data = (
+            <Paper sx={{ width: "100%", overflow: "hidden" }}>
+                <TableContainer
+                    sx={{
+                        width: "100%",
+                        maxHeight: window.innerWidth >= 600 ? 700 : 350,
+                    }}
+                >
+                    <Table stickyHeader aria-label="sticky table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell
+                                    key="0"
+                                    sx={{
+                                        fontWeight: "bold",
+                                        backgroundColor: "#f0f0f0",
+                                    }}
+                                >
+                                    Id
+                                </TableCell>
+                                <TableCell
+                                    key="1"
+                                    sx={{
+                                        fontWeight: "bold",
+                                        backgroundColor: "#f0f0f0",
+                                    }}
+                                >
+                                    Name
+                                </TableCell>
+                                <TableCell
+                                    key="2"
+                                    sx={{
+                                        fontWeight: "bold",
+                                        backgroundColor: "#f0f0f0",
+                                    }}
+                                >
+                                    Course Number
+                                </TableCell>
+                                <TableCell
+                                    key="3"
+                                    sx={{
+                                        fontWeight: "bold",
+                                        backgroundColor: "#f0f0f0",
+                                    }}
+                                >
+                                    Notes
+                                </TableCell>
+                                <TableCell
+                                    key="4"
+                                    sx={{
+                                        fontWeight: "bold",
+                                        backgroundColor: "#f0f0f0",
+                                    }}
+                                >
+                                    Approved Instructors
+                                </TableCell>
+                                <TableCell
+                                    key="5"
+                                    sx={{
+                                        fontWeight: "bold",
+                                        backgroundColor: "#f0f0f0",
+                                    }}
+                                >
+                                    Allowed Block Splits
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {allCourseOfferings.map((row, rowIndex) => (
+                                <TableRow key={rowIndex}>
+                                    <TableCell key="0">{row.id}</TableCell>
+                                    <TableCell key="1">{row.name}</TableCell>
+                                    <TableCell key="2">{row.courseNumber}</TableCell>
+                                    <TableCell key="3">{row.notes}</TableCell>
+                                    <TableCell key="4">
+                                        {row.approvedInstructors.map((instructorId) => (
+                                            <span key={instructorId}>
+                                                {getInstructorName(instructorId)}
+                                                <br />
+                                            </span>
+                                        ))}
+                                    </TableCell>
+                                    <TableCell key="5">
+                                        {JSON.stringify(row.allowedBlockSplits)}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
+        );
+    }
+    return (
+        <div>
+            <h2 className={styles.title}>Current Course Offerings</h2>
+            {data}
+        </div>
+    );
+}
+
+export default ViewUploadedCourseOfferingList;
