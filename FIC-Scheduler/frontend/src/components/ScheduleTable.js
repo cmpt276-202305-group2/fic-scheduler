33bc4192c9f9856fff16ce43a039189ad7f0c081
FuJen0980 <chiangjeff23@gmail.com>
2023-07-30 22:58:38 -0700

    added schedule table

--- FIC-Scheduler/frontend/src/components/ScheduleTable.js
+++ FIC-Scheduler/frontend/src/components/ScheduleTable.js
@@ -4,104 +4,114 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Button from "@mui/material/Button";
+
 import Paper from "@mui/material/Paper";
-import styles from "./FileImport.module.css";
+
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { CSVLink } from "react-csv";
+
 
 
 import { tokenConfig } from "../utils"
 
 export function ScheduleTable() {
-  const [fetchResult, setFetchResult] = useState(null);
+    const [scheduleData, setScheduleData] = useState(null);
 
-  useEffect(() => {
-    axios.get("api/schedules/latest", tokenConfig()).then(
-      (response) => { setFetchResult(response.data); },
-      (_) => { setFetchResult(null); });
-  }, [setFetchResult]);
+    useEffect(() => {
+        axios.get("api/schedules", tokenConfig()).then(
+            (response) => { setScheduleData(response.data); },
+            (_) => { setScheduleData(null); });
+    }, [setScheduleData]);
+    if (!scheduleData) {
+        return (
+            <div data-testid="schedule">
+                <div>No schedules generated!</div>
+            </div>
+        );
+    }
 
-  if ((fetchResult === null) || !(fetchResult.classScheduleAssignments instanceof Array)) {
+
+
+    // // Function to fetch block split names by IDs
+    // const fetchBlockSplitNames = (blockSplitIds) => {
+    //     const requests = blockSplitIds.map((id) => axios.get(`api/block-splits/${id}`, tokenConfig()));
+    //     axios.all(requests)
+    //         .then(axios.spread((...responses) => {
+    //             const blockSplitData = responses.map((response) => response.data);
+    //             // Create an object with block split names using the IDs as keys
+    //             const blockSplitNamesObject = {};
+    //             blockSplitData.forEach((blockSplit) => {
+    //                 blockSplitNamesObject[blockSplit.id] = blockSplit.name;
+    //             });
+    //             setBlockSplitNames(blockSplitNamesObject);
+    //         }))
+    //         .catch((error) => {
+    //             console.error("Error fetching block split names:", error);
+    //         });
+    // };
+
+    // Combine the scheduleData with csvData
+    const combinedData = scheduleData.flatMap((row) => {
+        return row.courses.map((courseObj) => ({
+            id: row.id,
+            name: row.name,
+            notes: row.notes,
+            semester: row.semester,
+            courseId: courseObj.course.id,
+            courseName: courseObj.course.name,
+            courseNumber: courseObj.course.courseNumber,
+            courseNotes: courseObj.course.notes,
+            approvedInstructors: courseObj.course.approvedInstructors,
+            allowedBlockSplits: courseObj.course.allowedBlockSplits,
+            blocks: courseObj.blocks,
+        }));
+    });
+
+    var data = (<div>No schedules</div>);
+    if (((scheduleData ?? null) !== null) && (scheduleData instanceof Array)) {
+        data = (
+            <div data-testid="schedule" style={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
+                <Paper sx={{ width: "100%", overflow: "hidden" }}>
+                    <TableContainer sx={{ width: "100%", maxHeight: window.innerWidth >= 600 ? 700 : 350 }}>
+                        <Table stickyHeader aria-label="sticky table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell key="0">ID</TableCell>
+                                    <TableCell key="1">Name</TableCell>
+                                    <TableCell key="2">Notes</TableCell>
+                                    <TableCell key="3">Semester</TableCell>
+                                    <TableCell key="5">Course Name</TableCell>
+                                    <TableCell key="7">Course Notes</TableCell>
+                                    <TableCell key="8">Approved Instructors</TableCell>
+                                    <TableCell key="9">Allowed Block Splits</TableCell>
+                                    <TableCell key="10">Blocks</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {combinedData.map((row, index) => (
+                                    <TableRow key={index}>
+                                        <TableCell key="0">{row.id}</TableCell>
+                                        <TableCell key="1">{row.name}</TableCell>
+                                        <TableCell key="2">{row.notes}</TableCell>
+                                        <TableCell key="3">{row.semester}</TableCell>
+                                        <TableCell key="5">{row.courseName}</TableCell>
+                                        <TableCell key="7">{row.courseNotes}</TableCell>
+                                        <TableCell key="8">{JSON.stringify(row.approvedInstructors)}</TableCell>
+                                        <TableCell key="9">{JSON.stringify(row.allowedBlockSplits)}</TableCell>
+                                        <TableCell key="10">{JSON.stringify(row.blocks)}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Paper>
+            </div>
+
+        );
+    }
     return (
-      <div data-testid="schedule">
-        <div>No schedules generated!</div>
-      </div>);
-  }
-
-  const csvData = fetchResult.classScheduleAssignments.map(row => ({
-    id: row.id,
-    courseNumber: row.courseNumber,
-    partOfDay: row.partOfDay,
-    classroom: row.classroom.roomNumber,
-    instructor: row.instructor.name,
-  }));
-
-  return (
-    <div
-      data-testid="schedule"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "flex-start",
-      }}>
-      <Paper sx={{ width: "100%", overflow: "hidden" }}>
-        <TableContainer
-          sx={{
-            width: "100%",
-            maxHeight: window.innerWidth >= 600 ? 700 : 350,
-          }}
-        >
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                <TableCell className={styles.cell} key="0">ID</TableCell>
-                <TableCell className={styles.cell} key="1">Course Number</TableCell>
-                <TableCell className={styles.cell} key="2">Time</TableCell>
-                <TableCell className={styles.cell} key="3">Classroom</TableCell>
-                <TableCell className={styles.cell} key="4">Instructor</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {fetchResult.classScheduleAssignments.map((row, rowIndex) => (
-                <TableRow key={rowIndex}>
-                  <TableCell key="0">{row.id}</TableCell>
-                  <TableCell key="1">{row.courseNumber}</TableCell>
-                  <TableCell key="2">{row.partOfDay}</TableCell>
-                  <TableCell key="3">{row.classroom.roomNumber}</TableCell>
-                  <TableCell key="4">{row.instructor.name}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Paper>
-      <label htmlFor="download-table">
-        <CSVLink
-          id="download-table"
-          data={csvData}
-          filename={"schedule.csv"}
-          className="btn btn-primary"
-          target="_blank"
-        >
-          <Button
-            id="upload-table"
-            variant="contained"
-            color="primary"
-            sx={{
-              color: "white",
-              backgroundColor: "#417A1A",
-              "&:hover": { backgroundColor: "#417A1A" },
-            }}
-            style={{ marginTop: 15 }}
-          >
-            Download Table
-          </Button>
-        </CSVLink>
-      </label>
-    </div>
-  );
-};
+        <div>{data}</div>
+    );
+}
 
 export default ScheduleTable;
--- FIC-Scheduler/frontend/src/pages/ViewFullSchedulePage.js
+++ FIC-Scheduler/frontend/src/pages/ViewFullSchedulePage.js
@@ -2,21 +2,22 @@ import { Navigate } from "react-router-dom";
 import styles from "./Common.module.css";
 import CheckAuth from "../components/CheckAuth";
 import Sidebar from "../components/Sidebar";
+import GenerateSchedule from "../components/GenerateSchedule";
 import ScheduleTable from "../components/ScheduleTable";
 
 function ViewFullSchedulePage() {
-  return (
-    <CheckAuth permittedRoles={["ADMIN", "COORDINATOR", "INSTRUCTOR"]} fallback={<Navigate to="/" replace />}>
-      <div className={styles.Container}>
-        <div className={styles.Sidebar}>
-          <Sidebar item="viewFullSchedule" />
-        </div>
-        <div className={styles.PageContent} data-testid="schedule">
-          <ScheduleTable />
-        </div>
-      </div>
-    </CheckAuth>
-  );
+    return (
+        <CheckAuth permittedRoles={["ADMIN", "COORDINATOR", "INSTRUCTOR"]} fallback={<Navigate to="/" replace />}>
+            <div className={styles.Container}>
+                <div className={styles.Sidebar}>
+                    <Sidebar item="viewFullSchedule" />
+                </div>
+                <div className={styles.PageContent} data-testid="schedule">
+                    <ScheduleTable />
+                </div>
+            </div>
+        </CheckAuth>
+    );
 }
 
 export default ViewFullSchedulePage;
