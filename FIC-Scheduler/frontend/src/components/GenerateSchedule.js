83be1be9397596e952f747beeed88e0a8ec8498e
FuJen0980 <chiangjeff23@gmail.com>
2023-08-01 06:50:41 -0700

    fixed generation button

--- FIC-Scheduler/frontend/src/components/GenerateSchedule.js
+++ FIC-Scheduler/frontend/src/components/GenerateSchedule.js
@@ -1,19 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
+import axios from "axios";
+import { tokenConfig } from "../utils";
 import ScheduleTable from "../components/ScheduleTable";
 
 function GenerateSchedule() {
-    const [showSchedule, setShowSchedule] = useState(false);
-    const [tableKey, setTableKey] = useState(0);
+    const [latestSemesterPlanId, setLatestSemesterPlanId] = useState(null);
+    const [hasFetchedLatestId, setHasFetchedLatestId] = useState(false);
+    const [showScheduleTable, setShowScheduleTable] = useState(false);
+    const [scheduleExists, setScheduleExists] = useState(false);
+    const [showErrorMessage, setShowErrorMessage] = useState(false);
 
-    const handleShowSchedule = () => {
-        setShowSchedule(true);
-        setTableKey(prevKey => prevKey + 1);
+    useEffect(() => {
+        if (!hasFetchedLatestId) {
+            axios
+                .get("api/semester-plans/latest", tokenConfig())
+                .then((response) => {
+                    const data = response.data;
+                    if (data.error) {
+                        console.log(data.error);
+                    } else {
+                        setLatestSemesterPlanId(data.id);
+                        setHasFetchedLatestId(true);
+                        setScheduleExists(data.scheduleExists);
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+        if (latestSemesterPlanId === null) {
+            setShowErrorMessage(true);
+        } else {
+            setShowErrorMessage(false);
+        }
+        axios
+            .get("api/schedules", tokenConfig())
+            .then((response) => {
+                const data = response.data;
+                if (data.length > 0) {
+                    setScheduleExists(true);
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [hasFetchedLatestId, latestSemesterPlanId]);
+
+    const handleGenerateSchedule = (event) => {
+        event.preventDefault();
+
+        if (scheduleExists) {
+            console.log("A schedule already exists.");
+            setShowScheduleTable(true);
+            return;
+        }
+
+        const postData = { semesterPlan: { id: latestSemesterPlanId } };
+        console.log("this is postData:", postData);
+        axios
+            .post("api/generate-schedule", postData, tokenConfig())
+            .then(
+                (response) => {
+                    setShowScheduleTable(true);
+                    setScheduleExists(true);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
     };
-
     return (
         <div>
             <Button
+                onClick={handleGenerateSchedule}
                 variant="contained"
                 color="primary"
                 sx={{
@@ -21,12 +81,14 @@ function GenerateSchedule() {
                     backgroundColor: "#417A1A",
                     "&:hover": { backgroundColor: "#417A1A" },
                 }}
-                onClick={handleShowSchedule}
-                style={{ marginBottom: 10 }}
+                style={{ marginBottom: 15 }}
             >
                 Generate Schedule
             </Button>
-            {showSchedule && <ScheduleTable key={tableKey} />}
+            {showErrorMessage && (
+                <div style={{ fontSize: 20 }}>Please Finish Importing Data Before Generating Schedule</div>
+            )}
+            {showScheduleTable && <ScheduleTable />}
         </div>
     );
 }
